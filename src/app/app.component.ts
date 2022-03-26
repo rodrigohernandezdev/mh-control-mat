bbe756baf9a3b64c5580f2036cc72e369fbd8f02
Rodrigo Hernández <rodrigo.hernandezdev@gmail.com>
2022-03-26 16:33:17 -0600

    feat(dev): Hiding the sidebar when screen is small.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import {Component, HostListener} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +7,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mh-control';
   sideBarOpen = true;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.sideBarOpen = event.target.innerWidth >= 500;
+  }
   sideBarToggle() {
     this.sideBarOpen = !this.sideBarOpen;
   }
